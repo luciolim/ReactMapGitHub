87938b5b4ea87b37390cd009e9d98abc7fcce5fe
luciolim <luciolim@gmail.com>
2021-04-14 01:33:52 +0100

    Sending the files

---
+++ src/api.ts
@@ -0,0 +1,10 @@
+const ACCESS_TOKEN_MAP_BOX = 
+"access_token=pk.eyJ1IjoibHVjaW9saW0iLCJhIjoiY2tuZ28xaDFtMDJ6bzJvcDEyNGZpejY2eSJ9.K2hggqxQsOslPK8jQ08OaQ";
+
+export const fetchLocalMapBox = (local: string) =>
+fetch(
+  `https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?${ACCESS_TOKEN_MAP_BOX}`
+).then(response => response.json()).then(data => data);
+
+export const fetchUserGithub = (login: string) =>
+fetch(`https://api.github.com/users/${login}`).then(response => response.json()).then(data => data);
